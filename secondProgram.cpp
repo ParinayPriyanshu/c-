7e6f2e66bd5a9982f6ea3e15bc50c0b71950c62b
ParinayPriyanshu <139611165+ParinayPriyanshu@users.noreply.github.com>
2024-03-14 20:00:08 +0530

    second Program

---
+++ secondProgram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//function
+void solve(){
+    int n;
+    cout << "Enter a number to print square of it: ";
+    cin >> n;
+    cout << n*n << endl;
+
+}
+int main(){
+    solve();
+    return 0;
+}
