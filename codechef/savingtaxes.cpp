63ebe8f8465565776d52a7002f135904f926cbbc
ParinayPriyanshu <139611165+ParinayPriyanshu@users.noreply.github.com>
2024-03-14 21:07:13 +0530

    saving taxes

---
+++ codechef/savingtaxes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T; 
+int X;
+int Y;
+
+cin>>T;
+for ( int i= 0; i<T; i++)
+{
+    cin>>X >> Y;
+    cout<< X-Y <<endl;
+}
+return 0;
+}
