7b96d6c3cb16a4463b3fadb5cea5395cc9d533a0
ParinayPriyanshu <139611165+ParinayPriyanshu@users.noreply.github.com>
2024-03-15 19:09:02 +0530

    masterchef final

---
+++ codechef/Mastercheffinals.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int x;
+
+cin>> x;
+
+for( int i = 0; i<x; i++){
+    int y;
+    cin>>y;
+if(y<=10){
+    cout<<"yes"<<endl;
+}
+else{
+    cout<<"no"<<endl;
+}}
+return 0;
+}
