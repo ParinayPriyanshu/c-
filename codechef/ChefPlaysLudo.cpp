2787a9ad6021a0888b5b2834826015827fe73938
ParinayPriyanshu <139611165+ParinayPriyanshu@users.noreply.github.com>
2024-03-17 09:04:00 +0530

    chefplayludo

---
+++ codechef/ChefPlaysLudo.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+cin>>T;
+while(T--){
+    int roll;
+    cin>>roll;
+    if (roll==6){
+        cout<< "yes"<<endl;
+    }else{
+        cout <<"No"<<endl;
+}
+}
+return 0;
+}
