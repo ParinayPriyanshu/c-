3fa1ecc3f5e595f28009a388bd0de984cc0039d8
ParinayPriyanshu <139611165+ParinayPriyanshu@users.noreply.github.com>
2024-03-18 23:15:25 +0530

    discount

---
+++ codechef/discount.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+    int x;
+    cin>>x;
+    int alice_needs_to_pay = 100 - x;
+    cout<<alice_needs_to_pay<<endl;
+    
+}
+return 0;
+}
