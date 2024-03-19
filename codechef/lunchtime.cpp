d0e5dc2c9c35d9aea9c7392fa234e0abf76e22a8
ParinayPriyanshu <139611165+ParinayPriyanshu@users.noreply.github.com>
2024-03-19 20:45:04 +0530

    lunch time

---
+++ codechef/lunchtime.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int T;
+    cin >> T;
+    
+    while (T--) {
+        int X;
+        cin >> X;
+        if (X >= 1 && X <= 4)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    
+    return 0;
+}
