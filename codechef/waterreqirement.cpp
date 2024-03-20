d602e20c4f3ab8a34202ddf84bf4a375a1a0f3a2
ParinayPriyanshu <139611165+ParinayPriyanshu@users.noreply.github.com>
2024-03-20 21:37:30 +0530

    water

---
+++ codechef/waterreqirement.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--){
+int n;
+cin>>n;
+
+int how_much_water = n*2;
+cout<<how_much_water<< endl;
+
+}
+return 0;
+}
